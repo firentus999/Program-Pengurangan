7ea48608e2c64ef13cd5c24264da1e2dfc5ccadd
Firentus Agustone Pasaribu <45529731+firentus999@users.noreply.github.com>
2018-12-12 09:20:36 +0700

    Program

---
+++ Program Pengurangan.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    float hasil;
+    cout<<"=========PROGRAM PENGURANGAN======="<<endl;
+    cout<<"masukan bilangan 1 :";
+    cin>>x;
+    cout<<"masukan bilangan 2 :";
+    cin>>y;
+    hasil=x-y;
+    cout<<"hasil Pengurangan adalah "<<hasil;
+    return 0;
+}
